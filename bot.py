12e327ce43b5e133d6afc8521ac32b6829ebe5f6
Lasiya <91351974+LasinduRukshan@users.noreply.github.com>
2022-11-28 12:37:45 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,79 @@
+import logging
+from telethon import TelegramClient, events, Button
+from decouple import config
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+
+# start the bot
+logging.info("Starting...")
+try:
+    apiid = config("APP_ID", cast=int)
+    apihash = config("API_HASH")
+    bottoken = config("BOT_TOKEN")
+    frm = config("FROM_CHANNEL", cast=lambda x: [int(_) for _ in x.split(" ")])
+    tochnls = config("TO_CHANNEL", cast=lambda x: [int(_) for _ in x.split(" ")])
+    datgbot = TelegramClient("bot", apiid, apihash).start(bot_token=bottoken)
+except:
+    logging.error("Environment vars are missing! Kindly recheck.")
+    logging.info("Bot is quiting...")
+    exit()
+
+
+@datgbot.on(events.NewMessage(pattern="/start"))
+async def _(event):
+    await event.reply(
+        f"Hi `{event.sender.first_name}`!\n\nI am a channel auto-post bot!! Read /help to know more!\n\nI can be used in only two channels (one user) at a time. Kindly deploy your own bot.\n\n[More bots](https://t.me/Danuma_admin_bot)..",
+        buttons=[
+            Button.url("Repo", url="https://github.com/LasinduRukshan/"),
+            Button.url("Dev", url="https://t.me/Danuma_admin_bot"),
+             Button.url("Chanel", url="https://t.me/nxtbots"),
+        ],
+        link_preview=False,
+    )
+
+
+@datgbot.on(events.NewMessage(pattern="/help"))
+async def helpp(event):
+    await event.reply(
+        "**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from [here](https://github.com/xditya/ChannelAutoForwarder).\n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @Danuma_admin_bot :)"
+    )
+
+
+@datgbot.on(events.NewMessage(incoming=True, chats=frm))
+async def _(event):
+    for tochnl in tochnls:
+        try:
+            if event.poll:
+                return
+            if event.photo:
+                photo = event.media.photo
+                await datgbot.send_file(
+                    tochnl, photo, caption=event.text, link_preview=False
+                )
+            elif event.media:
+                try:
+                    if event.media.webpage:
+                        await datgbot.send_message(
+                            tochnl, event.text, link_preview=False
+                        )
+                except Exception:
+                    media = event.media.document
+                    await datgbot.send_file(
+                        tochnl, media, caption=event.text, link_preview=False
+                    )
+                finally:
+                    return
+            else:
+                await datgbot.send_message(tochnl, event.text, link_preview=False)
+        except Exception as exc:
+            logging.error(
+                "TO_CHANNEL ID is wrong or I can't send messages there (make me admin).\nTraceback:\n%s",
+                exc,
+            )
+
+
+logging.info("Bot has started.")
+logging.info("Do visit @nxtbots..")
+datgbot.run_until_disconnected()
